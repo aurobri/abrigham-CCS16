8d46ef866f1a58effc3116a3fbbec655a8050c16
Aurora Brigham <aurorabrigham@172-16-10-148.dynapool.nyu.edu>
2016-02-01 18:42:42 -0500

    Uploading homework

---
+++ Assignment1/Black and White.js
@@ -0,0 +1,311 @@
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+}
+
+function draw() {
+  line(0,5,windowWidth,10); 
+  stroke (0)
+  strokeWeight(5);
+  
+  line(5,0,10,windowWidth); 
+  stroke(0);
+  strokeWeight(5);
+  
+  line(5,15,windowWidth,20);
+  stroke(0);
+  strokeWeight(5);
+  
+  line(15,5,20,windowHeight)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(10,25,windowWidth,30)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(25,10,30,windowHeight)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(15,35,windowWidth,40)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(35,15,40,windowHeight)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(20,45,windowWidth,50)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(45,20,50,windowHeight)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(25,55,windowWidth,60)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(55,25,60,windowHeight)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(30,65,windowWidth,70)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(65,30,70,windowHeight)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(35,75,windowWidth,80) 
+  stroke(0);
+  strokeWeight(5);
+  
+  line(75,35,80,windowHeight)
+  stroke(0);
+  strokeWeight(5);
+  
+  // RED Lines
+  line(40,85,windowWidth,90)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(45,95,windowWidth,100)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(50,105,windowWidth,110)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(55,115,windowWidth,120)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(60,125,windowWidth,130)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(65,135,windowWidth,140)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(70,145,windowWidth,150)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(75,155,windowWidth,160)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(80,165,windowWidth,170)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(85,175,windowWidth,180)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(90,185,windowWidth,190);
+  stroke(0);
+  strokeWeight(5);
+  
+  line(95,195,windowWidth,200);
+  stroke(0);
+  strokeWeight(5);
+  
+  //BLUE Lines
+  line(85,40,90,windowHeight)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(95,45,100,windowHeight)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(105,50,110,windowHeight)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(115,55,120,windowHeight)
+  stroke(0);
+  strokeWeight (5)
+  
+  line(125,60,130,windowHeight)
+  stroke (0);
+  strokeWeight(5);
+  
+  line(135,65,140,windowHeight)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(145,70,150,windowHeight)
+  stroke (0);
+  strokeWeight(5);
+  
+  line(155,75,160,windowHeight)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(165,80,170,windowHeight)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(175,85,180,windowHeight)
+  stroke (0);
+  strokeWeight(5);
+  
+  line(185,90,190,windowHeight)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(195,95,200,windowHeight)
+  stroke(0);
+  strokeWeight(5);
+  
+  //Cross
+  
+  line(400,0,0,400)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(420,0,0,420)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(440,0,0,440)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(460,0,0,460)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(480,0,0,480)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(500,0,0,500)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(520,0,0,520)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(540,0,0,540)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(560,0,0,560)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(580,0,0,580)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(600,0,0,600)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(620,0,0,620)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(640,0,0,640)
+  stroke(0);
+  strokeWeight(5);
+  
+  //Middle
+  line(740,0,0,740)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(760,0,0,760)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(780,0,0,780)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(800,0,0,800)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(820,0,0,820)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(840,0,0,840)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(860,0,0,860)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(880,0,0,880)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(900,0,0,900)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(1000,0,0,1000)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(1020,0,0,1020)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(1040,0,0,1040)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(1060,0,0,1060)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(1080,0,0,1080)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(1100,0,0,1100)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(1120,0,0,1120)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(1140,0,0,1140)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(1160,0,0,1160)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(1180,0,0,1180)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(1200,0,0,1200)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(1220,0,0,1220)
+  stroke(0);
+  strokeWeight(5);
+  
+  line(1240,0,0,1240)
+  stroke(0);
+  strokeWeight(5);
+  
+}
