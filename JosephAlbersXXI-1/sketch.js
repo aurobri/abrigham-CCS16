881c4446d1288bbefb253f664aee7fecb136b607
Aurora Brigham <aurorabrigham@auroras-air.home>
2016-05-09 13:43:49 -0400

    Second Joseph Albers drawing week 4

---
+++ JosephAlbersXXI-1/sketch.js
@@ -0,0 +1,50 @@
+function setup() {
+  createCanvas (500,700);
+}
+
+function draw() {
+  background(255);
+  //Top squares
+  noStroke();
+  fill(95,45,45);
+  rect (90,30,150,90);
+  noStroke();
+  fill(32,45,81);
+  rect (240,30,150,90);
+  //2nd square
+  noStroke();
+  fill(38,33,69);
+  rect (110,130,150,90);
+  noStroke();
+  fill(113,45,45);
+  rect (260,130,150,90);
+  //3rd squares
+  noStroke();
+  fill(125,40,34);
+  rect (90,230,150,90);
+  noStroke();
+  fill(34,52,74);
+  rect (240,230,150,90);
+  //4th squares
+  noStroke();
+  fill(26,29,51);
+  rect (110,330,150,90);
+  noStroke();
+  fill(112,41,35);
+  rect (260,330,150,90);
+  //5th squares
+  noStroke();
+  fill(94,41,48);
+  rect (90,430,150,90);
+  noStroke();
+  fill(42,63,59);
+  rect (240,430,150,90);
+  //6th squares
+  noStroke();
+  fill(50,75,83);
+  rect (110,530,150,90);
+  noStroke();
+  fill(144,74,61);
+  rect (260,530,150,90);
+  
+}
